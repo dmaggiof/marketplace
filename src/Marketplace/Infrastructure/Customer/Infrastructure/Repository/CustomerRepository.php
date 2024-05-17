ec5c96af14c67d551575f57d2e8920356583c07c
userver <amfolgar@gmail.com>
2024-05-17 23:10:02 +0200

    Refactor

--- src/Marketplace/Infrastructure/Customer/Infrastructure/Repository/CustomerRepository.php
+++ src/Marketplace/Infrastructure/Customer/Infrastructure/Repository/CustomerRepository.php
@@ -25,26 +25,4 @@ class CustomerRepository extends ServiceEntityRepository implements CustomerRepo
         $this->manager->persist($customer);
         $this->manager->flush();
     }
-    //    /**
-    //     * @return Customer[] Returns an array of Customer objects
-    //     */
-        public function findByExampleField($value): array
-        {
-            return $this->createQueryBuilder('c')
-                ->andWhere('c.name = :val')
-                ->setParameter('val', $value)
-                ->setMaxResults(1)
-                ->getQuery()
-                ->getResult()
-            ;
-        }
-    //    public function findOneBySomeField($value): ?Customer
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
