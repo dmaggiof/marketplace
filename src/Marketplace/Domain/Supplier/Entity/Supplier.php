957a85985a80b0c8c67a8c032f17cc414bf58c66
userver <amfolgar@gmail.com>
2024-05-19 20:16:52 +0200

    Se añaden ficheros de proveedores

---
+++ src/Marketplace/Domain/Supplier/Entity/Supplier.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Marketplace\Domain\Supplier\Entity;
+
+use Marketplace\Infrastructure\Supplier\Repository\SupplierRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SupplierRepository::class)]
+class Supplier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cif = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCif(): ?string
+    {
+        return $this->cif;
+    }
+
+    public function setCif(string $cif): static
+    {
+        $this->cif = $cif;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
